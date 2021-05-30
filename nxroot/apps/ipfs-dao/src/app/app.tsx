b54a637b49c7ca4889096206e54380ae174026ff
gavin.doughtie <gavin.doughtie@gmail.com>
2021-05-30 11:40:36 -0700

    adding metamask integrations

--- nxroot/apps/ipfs-dao/src/app/app.tsx
+++ nxroot/apps/ipfs-dao/src/app/app.tsx
@@ -1,17 +1,45 @@
 import styles from './app.module.css';
 import { Filezone } from './filezone/filezone';
 import logo from '../assets/IPFS-DAO-Logo.png';
+import { generatePrivateKey } from './helpers/crypto';
+import { useState, ChangeEventHandler, ChangeEvent } from 'react';
 
-export interface IAppProps {keytext: string};
+export interface IAppProps {
+  keytext: string;
+}
 export function App(props: IAppProps) {
+  const [secret, setSecret] = useState('');
   const keytext = props.keytext;
+  async function updatePrivateKey() {
+    console.log('updatePrivateKey');
+    if (secret) {
+      const key = await generatePrivateKey(secret);
+      console.log(`key: ${key}`);
+    }
+  }
+  const handleChange: ChangeEventHandler = (e: ChangeEvent<HTMLInputElement>) =>
+    setSecret(e.target?.value);
+
   return (
     <div className={styles.app}>
       <header className="flex">
         <img src={logo} alt="ipfs dao logo"></img>
       </header>
+      <div className="login">
+        <label htmlFor="loginInput">label="Human Login"</label>
+        <input
+          id="loginInput"
+          title="Combine a private secret with Metamask signing to generate ed25519 private key (Enter a secret. View console.)"
+          name="secret"
+          value={secret}
+          placeholder="Secret"
+          type="password"
+          onChange={handleChange}
+        />
+        <button onClick={() => updatePrivateKey()}>Login with Metamask</button>
+      </div>
       <main>
-        <Filezone keytext={keytext}/>
+        <Filezone keytext={keytext} />
       </main>
     </div>
   );
--- nxroot/apps/ipfs-dao/src/app/helpers/crypto.ts
+++ nxroot/apps/ipfs-dao/src/app/helpers/crypto.ts
@@ -1,3 +1,7 @@
+import { PrivateKey } from '@textile/hub';
+import { BigNumber, providers, utils, Signer } from 'ethers';
+import { hashSync } from 'bcrypt';
+
 export async function generateKey() {
   const key = await window.crypto.subtle.generateKey(
     {
@@ -95,4 +99,152 @@ export async function decrypt(
     buffer
   );
   return decrypted;
+}
+
+export type WindowInstanceWithEthereum = Window &
+  typeof globalThis & { ethereum?: providers.ExternalProvider };
+
+export class StrongType<Definition, Type> {
+  private _type: Definition;
+  constructor(public value?: Type) {
+    /* eslint-disable-next-line */
+    this._type = undefined as any;
+  }
+}
+export class EthereumAddress extends StrongType<'ethereum_address', string> {}
+
+export function generateMessageForEntropy(
+  ethereum_address: EthereumAddress,
+  application_name: string,
+  secret: string
+): string {
+  return (
+    '******************************************************************************** \n' +
+    'READ THIS MESSAGE CAREFULLY. \n' +
+    'DO NOT SHARE THIS SIGNED MESSAGE WITH ANYONE OR THEY WILL HAVE READ AND WRITE \n' +
+    'ACCESS TO THIS APPLICATION. \n' +
+    'DO NOT SIGN THIS MESSAGE IF THE FOLLOWING IS NOT TRUE OR YOU DO NOT CONSENT \n' +
+    'TO THE CURRENT APPLICATION HAVING ACCESS TO THE FOLLOWING APPLICATION. \n' +
+    '******************************************************************************** \n' +
+    'The Ethereum address used by this application is: \n' +
+    '\n' +
+    ethereum_address.value +
+    '\n' +
+    '\n' +
+    '\n' +
+    'By signing this message, you authorize the current application to use the \n' +
+    'following app associated with the above address: \n' +
+    '\n' +
+    application_name +
+    '\n' +
+    '\n' +
+    '\n' +
+    'The hash of your non-recoverable, private, non-persisted password or secret \n' +
+    'phrase is: \n' +
+    '\n' +
+    secret +
+    '\n' +
+    '\n' +
+    '\n' +
+    '******************************************************************************** \n' +
+    'ONLY SIGN THIS MESSAGE IF YOU CONSENT TO THE CURRENT PAGE ACCESSING THE KEYS \n' +
+    'ASSOCIATED WITH THE ABOVE ADDRESS AND APPLICATION. \n' +
+    'AGAIN, DO NOT SHARE THIS SIGNED MESSAGE WITH ANYONE OR THEY WILL HAVE READ AND \n' +
+    'WRITE ACCESS TO THIS APPLICATION. \n' +
+    '******************************************************************************** \n'
+  );
+}
+
+function getExtermalProvider(): providers.ExternalProvider {
+  const externalProvider = (window as WindowInstanceWithEthereum).ethereum;
+  if (externalProvider) {
+    return externalProvider;
+  } else {
+    throw new Error(
+      'Ethereum is not connected. Please download Metamask from https://metamask.io/download.html'
+    );
+  }
+}
+
+async function getSigner(): Promise<Signer> {
+  const externalProvider = getExtermalProvider();
+
+  console.debug('Initializing web3 provider...');
+
+  const provider = new providers.Web3Provider(externalProvider);
+  return provider.getSigner();
+}
+
+export async function getAddressAndSigner(): Promise<{
+  address: EthereumAddress;
+  signer: Signer;
+}> {
+  const signer = await getSigner();
+  const externalProvider = getExtermalProvider();
+  const request = externalProvider.request;
+  if (!request) {
+    throw new Error('No request available on provider');
+  }
+  const accounts = await request({ method: 'eth_requestAccounts' });
+  if (accounts.length === 0) {
+    throw new Error(
+      'No account is provided. Please provide an account to this application.'
+    );
+  }
+
+  const address = new EthereumAddress(accounts[0]);
+
+  return { address, signer };
+}
+
+export async function generatePrivateKey(
+  userSecret: string | Buffer
+): Promise<PrivateKey> {
+  const metamask = await getAddressAndSigner();
+  // avoid sending the raw secret by hashing it first
+  const secret = hashSync(userSecret, 10);
+  const message = generateMessageForEntropy(
+    metamask.address,
+    'ipfs-dao',
+    secret
+  );
+  const signedText = await metamask.signer.signMessage(message);
+  const hashString = utils.keccak256(signedText);
+  let hashArray: number[] = [];
+  if (!hashString) {
+    throw new Error(
+      'No account is provided. Please provide an account to this application.'
+    );
+  } else {
+    // The following line converts the hash in hex to an array of 32 integers.
+    const matches = hashString.replace('0x', '').match(/.{2}/g);
+    if (matches) {
+      hashArray = matches.map((hexNoPrefix) =>
+        BigNumber.from('0x' + hexNoPrefix).toNumber()
+      );
+    }
+    if (hashArray.length !== 32) {
+      throw new Error(
+        'Hash of signature is not the correct size! Something went wrong!'
+      );
+    }
+  }
+  const identity = PrivateKey.fromRawEd25519Seed(Uint8Array.from(hashArray));
+  console.log(identity.toString());
+
+  createNotification(identity);
+
+  // Your app can now use this identity for generating a user Mailbox, Threads, Buckets, etc
+  return identity;
+}
+
+function createNotification(identity: PrivateKey) {
+  console.dir({
+    name: 'create-notification',
+    detail: {
+      id: 1,
+      description: `PubKey: ${identity.public.toString()}. Your app can now generate and reuse this users PrivateKey for creating user Mailboxes, Threads, and Buckets.`,
+      timeout: 5000,
+    },
+  });
 }
